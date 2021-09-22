4d9e4e92a7e9fa323dc74a740d4adf6997f6b20c
j.belova <fridadet@gmail.com>
2021-09-22 16:29:17 +0300

    OG-98 deleted prcess-localisation and protein-activity-object

--- app/models/GeneToProteinActivity.php
+++ app/models/GeneToProteinActivity.php
@@ -54,47 +54,4 @@ class GeneToProteinActivity extends common\GeneToProteinActivity
         return $result;
     }
 
-    /**
-     * @param array $modelArrays
-     * @param int $geneId
-     * @throws \Throwable
-     * @throws \yii\db\StaleObjectException
-     */
-    public static function saveMultipleForGene(array $modelArrays, int $geneId)
-    {
-        foreach ($modelArrays as $id => $modelArray) {
-            if($modelArray['protein_activity_id'] && $modelArray['protein_activity_object_id']) {
-                if(is_numeric($id)) {
-                    $modelAR = self::findOne($id);
-                } else {
-                    $modelAR = new self();
-                }
-                if ($modelArray['delete'] === '1' && $modelAR instanceof ActiveRecord) {
-                    $modelAR->delete();
-                    continue;
-                }
-                $modelAR->setAttributes($modelArray);
-                if(!is_numeric($modelArray['protein_activity_object_id'])) {
-                    $arProteinActivityObject = ProteinActivityObject::createFromNameString($modelArray['protein_activity_object_id']);
-                    $modelAR->protein_activity_object_id = $arProteinActivityObject->id;
-                }
-                if(!is_numeric($modelArray['process_localization_id'])) {
-                    $arProcessLocalization = ProcessLocalization::createFromNameString($modelArray['process_localization_id']);
-                    $modelAR->process_localization_id = $arProcessLocalization->id;
-                }
-                if(!is_numeric($modelArray['protein_activity_id'])) {
-                    $arProteinActivity = ProteinActivity::createFromNameString($modelArray['protein_activity_id']);
-                    $modelAR->protein_activity_id = $arProteinActivity->id;
-                }
-                $modelAR->gene_id = $geneId;
-                if($modelAR->process_localization_id === '') {
-                    $modelAR->process_localization_id = null;
-                }
-                if(!$modelAR->save()) {
-                    var_dump($modelAR->errors); die;
-                }
-            }
-        }
-    }
-
 }
