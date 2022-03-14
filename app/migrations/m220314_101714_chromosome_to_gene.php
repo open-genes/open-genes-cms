9e60745868dc4d5c8534bd223e7b693b87f2fc62
j.belova <fridadet@gmail.com>
2022-03-14 13:21:21 +0300

    OG-689 chromosome_to_gene

--- app/migrations/m220314_101714_chromosome_to_gene.php
+++ app/migrations/m220314_101714_chromosome_to_gene.php
@@ -12,7 +12,8 @@ class m220314_101714_chromosome_to_gene extends Migration
      */
     public function safeUp()
     {
-
+        $this->addColumn('gene', 'chromosome', \yii\db\Schema::TYPE_INTEGER);
+        $this->createIndex('in_gene_chromosome', 'gene', 'chromosome');
     }
 
     /**
@@ -20,9 +21,8 @@ class m220314_101714_chromosome_to_gene extends Migration
      */
     public function safeDown()
     {
-        echo "m220314_101714_chromosome_to_gene cannot be reverted.\n";
-
-        return false;
+        $this->dropIndex('in_gene_chromosome', 'gene');
+        $this->dropColumn('gene', 'chromosome');
     }
 
     /*
