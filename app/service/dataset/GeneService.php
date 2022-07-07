dfeba19d1836fedca068a3cf5c6843e1f44a74d4
Arian Rin <arianrin@ArianRin.local>
2022-07-07 22:59:21 +0800

    80. add migrate copy data to gen

---
+++ app/service/dataset/GeneService.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace app\service\dataset;
+
+use app\models\Gene;
+
+class GeneService
+{
+    /** @var LifespanExperimentService */
+    private $lifespanExperimentService;
+
+    /** @var GeneInterventionToVitalProcessService */
+    private $geneInterventionToVitalProcessService;
+
+    /** @var AgeRelatedChangeService */
+    private $ageRelatedChangeService;
+
+    public function __construct() {
+        $this->lifespanExperimentService = new LifespanExperimentService();
+        $this->ageRelatedChangeService = new GeneInterventionToVitalProcessService();
+        $this->geneInterventionToVitalProcessService = new AgeRelatedChangeService();
+    }
+
+
+    public function copyDataToData(array $data) {
+        foreach ($data as $item) {
+            $symbol = $item[0];
+            $symbols = $item[1] ? explode(",", $item[1]) : [];
+            $conditions = $item[2] ? explode(",", $item[2]) : [];
+            $geneSymbols = [];
+            $gene = Gene::find()->where(['symbol' => $symbol])->one();
+
+            if (!empty($gene) && !empty($conditions) && !empty($symbols)) {
+                $geneSymbols = Gene::find()->where(['in', 'symbol', $symbols])->all();
+
+                // TODO: феолетовый
+                if (!empty($gene->lifespanExperiments)) {
+                    foreach ($gene->lifespanExperiments as $lifespanExperiment) {
+                        if ($generalLifespanExperiment = $lifespanExperiment->generalLifespanExperiment) {
+                            if (!empty($generalLifespanExperiment->model_organism_id)) {
+                                if (in_array($generalLifespanExperiment->modelOrganism->name_ru, $conditions)) {
+                                    $this
+                                        ->lifespanExperimentService
+                                        ->checkDuplicateAndSave($geneSymbols, $lifespanExperiment);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                // TODO: зеленный
+                if (!empty($gene->geneInterventionToVitalProcesses)) {
+                    foreach ($gene->geneInterventionToVitalProcesses as $geneInterventionToVitalProcess) {
+                        if (!empty($geneInterventionToVitalProcess->model_organism_id)) {
+                            if (in_array($geneInterventionToVitalProcess->modelOrganism->name_ru, $conditions)) {
+                                $this
+                                    ->geneInterventionToVitalProcessService
+                                    ->checkDuplicateAndSave($geneSymbols, $geneInterventionToVitalProcess);
+                            }
+                        }
+                    }
+                }
+
+                // TODO: голубой
+                if (!empty($gene->ageRelatedChanges)) {
+                    foreach ($gene->ageRelatedChanges as $ageRelatedChange) {
+                        if (!empty($ageRelatedChange->model_organism_id)) {
+                            if (in_array($ageRelatedChange->modelOrganism->name_ru, $conditions)) {
+                                $this
+                                    ->ageRelatedChangeService
+                                    ->checkDuplicateAndSave($geneSymbols, $ageRelatedChange);
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
+}
