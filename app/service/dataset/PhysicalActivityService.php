b8b7d5a1edeb81a8ab8d2ec0862ef27bac1c3fe5
Arian Rin <arianrin@ArianRin.local>
2022-07-26 09:52:35 +0800

    82. Fix change data of file

--- app/service/dataset/PhysicalActivityService.php
+++ app/service/dataset/PhysicalActivityService.php
@@ -24,30 +24,30 @@ class PhysicalActivityService
                     continue;
                 }
 
-                $sample = strtolower($data[3]);
+                $sample = strtolower($data[5]);
                 $tissue = Sample::find()->where(['name_en' => $sample])->one();
                 if (empty($tissue)) {
                     continue;
                 }
 
-                $expressionEvaluationBy = $data[6];
+                $expressionEvaluationBy = $data[12];
                 $expressionEvaluation = ExpressionEvaluation::find()->where(['name_en' => $expressionEvaluationBy])->one();
                 if (empty($expressionEvaluation)) {
                     continue;
                 }
 
-                $modelOrganismName = $data[10];
+                $modelOrganismName = $data[6];
                 $modelOrganism = ModelOrganism::find()->where(['name_en' => $modelOrganismName])->one();
                 if (empty($modelOrganism)) {
                     continue;
                 }
 
-                $organismLineName = !empty($data[11]) ? $data[11] == 'n/a' ? null : $data[11] : null;
+                $organismLineName = !empty($data[8]) ? $data[8] == 'n/a' ? null : $data[8] : null;
                 if ($organismLineName) {
                     $organismLine = OrganismLine::find()->where(['name_en' => $organismLineName])->one();
                 }
 
-                $organismSexName = $data[12];
+                $organismSexName = $data[7];
                 $organismSex = OrganismSex::find()->where(['name_en' => $organismSexName])->one();
                 if (empty($organismSex)) {
                     continue;
@@ -60,18 +60,18 @@ class PhysicalActivityService
                 $physicalActivity->model_organism_id = $modelOrganism->id;
                 $physicalActivity->organism_line_id = !empty($organismLineName) && $organismLine ? $organismLine->id : null;
                 $physicalActivity->organism_sex_id = $organismSex->id;
-                $physicalActivity->p_value = $data[2];
-                $physicalActivity->after_sport_result = $data[4];
-                $physicalActivity->time_point = $data[7];
-                $physicalActivity->training_regimen = $data[9];
-                $physicalActivity->sportsman = $data[13];
-                $physicalActivity->age = $data[14];
-                $physicalActivity->age_units = $data[15];
-                $physicalActivity->experiment_groups_quantity = $data[16];
-                $physicalActivity->link = $data[17];
-                $physicalActivity->expression_change_log = $data[20];
+                $physicalActivity->p_value = $data[4];
+                $physicalActivity->after_sport_result = $data[1];
+                $physicalActivity->time_point = $data[13];
+                $physicalActivity->training_regimen = $data[15];
+                $physicalActivity->sportsman = $data[16];
+                $physicalActivity->age = $data[9];
+                $physicalActivity->age_units = $data[10];
+                $physicalActivity->experiment_groups_quantity = $data[17];
+                $physicalActivity->link = $data[18];
+                $physicalActivity->expression_change_log = $data[3];
 
-                $measurementMethodNames = explode(',', trim($data[5]));
+                $measurementMethodNames = explode(',', trim($data[11]));
                 foreach ($measurementMethodNames as $name) {
                     $measurementMethod = MeasurementMethod::find()->where(['name_en' => $name])->one();
                     if (!empty($measurementMethod)) {
