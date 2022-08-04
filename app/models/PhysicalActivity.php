2a4ce46ba3f0cbf79c41048fb0e32ecf344c2746
Constantine <kon.rafikov@yandex.ru>
2022-08-04 18:52:15 -0300

    #82. Set proper names for dataset and DB columns

--- app/models/PhysicalActivity.php
+++ app/models/PhysicalActivity.php
@@ -8,14 +8,15 @@ use yii\behaviors\TimestampBehavior;
  * PhysicalActivity
  *
  * @property string $p_value [varchar(255)]
- * @property string $after_sport_result [varchar(255)]
- * @property string $time_point [varchar(255)]
+ * @property string $result [varchar(255)]
+ * @property string $measurement_taken [varchar(255)]
  * @property string $training_regimen [varchar(255)]
- * @property string $sportsman [varchar(255)]
+ * @property string $participants [varchar(255)]
+ * @property string $duration [varchar(255)]
  * @property string $age [varchar(255)]
  * @property string $age_units [varchar(255)]
  * @property string $experiment_groups_quantity [varchar(255)]
- * @property string $link [varchar(255)]
+ * @property string $reference [varchar(255)]
  * @property string $expression_change_log [varchar(255)]
  *
  * @property Gene $gene
