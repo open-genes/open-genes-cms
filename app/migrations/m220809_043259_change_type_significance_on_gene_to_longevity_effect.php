27e730e8a85618ecdfb23da6cdd4b63fbcd76742
Arian Rin <arianrin@ArianRin.local>
2022-08-09 12:40:35 +0800

    93. add pink experiments

---
+++ app/migrations/m220809_043259_change_type_significance_on_gene_to_longevity_effect.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220809_043259_change_type_significance_on_gene_to_longevity_effect
+ */
+class m220809_043259_change_type_significance_on_gene_to_longevity_effect extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->alterColumn('gene_to_longevity_effect', 'significance', $this->string()->null());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->alterColumn('gene_to_longevity_effect','significance', $this->integer());
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m220809_043259_change_type_significance_on_gene_to_longevity_effect cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
