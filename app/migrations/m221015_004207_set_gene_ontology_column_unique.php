f9a9770b5aeb3fc2780c688f097b66dd04686813
Arian Rin <arianrin@ArianRin.local>
2022-10-15 10:27:21 +0800

    OG-130: fix. migration file

--- app/migrations/m221015_004207_set_gene_ontology_column_unique.php
+++ app/migrations/m221015_004207_set_gene_ontology_column_unique.php
@@ -12,6 +12,14 @@ class m221015_004207_set_gene_ontology_column_unique extends Migration
      */
     public function safeUp()
     {
+        $this->execute('DELETE FROM gene_ontology_to_aging_mechanism_visible');
+        $this->execute('DELETE FROM aging_mechanism_to_gene_ontology');
+        $this->execute('DELETE FROM gene_to_ontology');
+        $this->execute('DELETE FROM gene_ontology_relation');
+        $this->execute('DELETE FROM aging_mechanism');
+        $this->execute('DELETE FROM gene_ontology');
+
+
         $this->execute('ALTER TABLE gene_ontology MODIFY COLUMN ontology_identifier varchar(255) NOT NULL;');
         $this->execute('ALTER TABLE gene_ontology ADD CONSTRAINT uniq_ontology_identifier UNIQUE (ontology_identifier);');
     }
