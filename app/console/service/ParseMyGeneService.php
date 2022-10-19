e05465e0c05796b808b5d6d315d76f4517dbe8be
Constantine <kon.rafikov@yandex.ru>
2022-10-19 01:45:18 -0300

    OG-918. Change the request to get data for retired genes

--- app/console/service/ParseMyGeneService.php
+++ app/console/service/ParseMyGeneService.php
@@ -43,7 +43,7 @@ class ParseMyGeneService implements ParseMyGeneServiceInterface
                     continue;
                 }
                 echo "{$arGene->id} {$arGene->ncbi_id} {$arGene->symbol} ({$counter} from {$count}): ";
-                $url = $this->apiUrl . 'gene/' . $arGene->ncbi_id . '?fields=symbol%2Cname%2Centrezgene%2Calias%2Csummary';
+                $url = $this->apiUrl . 'query?q=' . 'retired:' . $arGene->ncbi_id . '&fields=symbol,name,entrezgene,alias,summary&species=human';
                 $response = $this->httpClient->createRequest()
                     ->setUrl($url)
                     ->send();
