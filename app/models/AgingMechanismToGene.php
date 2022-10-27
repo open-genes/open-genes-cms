9b6693847999ea9fdce72cd090eb742949ffe2e3
Arian Rin <arianrin@ArianRin.local>
2022-10-28 06:53:19 +0800

    134: mechanism with gene, and ui

---
+++ app/models/AgingMechanismToGene.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\models;
+
+use app\models\behaviors\ChangelogBehavior;
+use app\models\traits\RuEnActiveRecordTrait;
+use Yii;
+
+/**
+ * This is the model class for table "aging_mechanism_to_gene".
+ *
+ * @property int $id
+ * @property int $gene_id
+ * @property int $aging_mechanism_id
+ *
+ * @property AgingMechanism $agingMechanism
+ * @property Gene $geneOntology
+ */
+class AgingMechanismToGene extends \app\models\common\AgingMechanismToGene
+{
+    use RuEnActiveRecordTrait;
+
+    public $name;
+
+    /**
+    * {@inheritdoc}
+    */
+    public function behaviors()
+    {
+        return [
+            ChangelogBehavior::class
+        ];
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'gene_id' => Yii::t('app', 'Gene ID'),
+            'aging_mechanism_id' => Yii::t('app', 'Aging Mechanism ID'),
+        ];
+    }
+
+
+    /**
+    * Gets query for [[AgingMechanism]].
+    *
+    * @return \yii\db\ActiveQuery
+    */
+    public function getAgingMechanism()
+    {
+        return $this->hasOne(AgingMechanism::class, ['id' => 'aging_mechanism_id']);
+    }
+
+    /**
+    * Gets query for [[Gene]].
+    *
+    * @return \yii\db\ActiveQuery
+    */
+    public function getGene()
+    {
+        return $this->hasOne(Gene::class, ['id' => 'gene_id']);
+    }
+
+}
